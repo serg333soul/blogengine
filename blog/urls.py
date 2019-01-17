04b9174f673dc700bda5f6f1a954051206b5713f
serg333soul <serg333soul@gmail.com>
2019-01-17 17:51:33 +0200

    work

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url')
 ]

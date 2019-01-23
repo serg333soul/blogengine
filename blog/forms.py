85e45cce40996e746762220731ca722a6cb53eb3
serg333soul <serg333soul@gmail.com>
2019-01-23 18:02:32 +0200

    lesson_7 end

--- blog/forms.py
+++ blog/forms.py
@@ -2,22 +2,36 @@ from django import forms
 from .models import Tag
 from django.core.exceptions import ValidationError
 
-class TagForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    slug = forms.CharField(max_length=50)
+class TagForm(forms.ModelForm):
+    # title = forms.CharField(max_length=50)
+    # slug = forms.CharField(max_length=50)
+    #
+    # title.widget.attrs.update({'class': 'form-control'})
+    # slug.widget.attrs.update({'class': 'form-control'})
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'})
+
+        }
 
     def clean_slug(self):
         new_slug = self.cleaned_data['slug'].lower()
 
         if new_slug == 'create':
             raise ValidationError('This name is reserved. Try another name.')
-        return new_slug    
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError('Slug must be unique. We have {} slug already'.format(new_slug))
+
+        return new_slug
 
 
 
-    def save(self):
-        new_tag = Tag.objects.create(
-            title=self.cleaned_data['title'],
-            slug=self.cleaned_data['slug']
-            )
-        return new_tag
+    # def save(self):
+    #     new_tag = Tag.objects.create(
+    #         title=self.cleaned_data['title'],
+    #         slug=self.cleaned_data['slug']
+    #         )
+    #     return new_tag
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Post, Tag
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
@@ -42,3 +43,11 @@ class TagCreate(View):
     def get(self, request):
         form = TagForm()
         return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})         

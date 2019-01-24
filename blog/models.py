46d1a110e8edf5388497c9b2ac71eacc9cf34b4e
serg333soul <serg333soul@gmail.com>
2019-01-24 16:47:12 +0200

    pycache

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 
+from django.utils.text import slugify
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
